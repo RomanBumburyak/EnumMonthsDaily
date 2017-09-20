4e195194c1caffde2c94288d463214f345c708b0
RomanBumburyak <romanbumburyak@gmail.com>
2017-09-20 08:43:55 -0400

    Third commit

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,10 +1,25 @@
 package com.example;
+import com.example.common.Month;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args){
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please choose one of the months");    /// using scanner to find on the months.
+
+        for (Month month : Month.values()) {
+            System.out.println(month.getEnglishName());
+
+        }
+        String userInput = scanner.nextLine();
 
 
+
+        ArrayList<String> months = new ArrayList<>();
 
 
 
--- src/com/example/common/Month.java
+++ src/com/example/common/Month.java
@@ -2,9 +2,36 @@ package com.example.common;
 
 public enum Month {
 
-    String[] monthNames = new String[12];
+    ///enumeration for the days of the week:
 
+
+    JANUARY("JAN"),
+    FEBRUARY("FEB"),
+    MARCH("MARCH") ,
+    APRIL("APRIL"),
+    MAY("MAY") ,
+    JUNE( "JUNE"),
+    JULY("JULY"),
+    AUGUST( "AUGUST"),
+    SEPTEMBER("SEPTEMBER"),
+    OCTOBER("OCTOBER"),
+    NOVEMBER("NOVEMBER"),
+    DECEMBER("DECEMBER");
+
+    private String englishName;
+
+    Month(String englishName) {
+        this.englishName = englishName;
+    }
+
+    ////This is my getter:
+
+    public String getEnglishName() {
+        return englishName;
+    }
 
 
 
 }
+
+
