3e9f31726b660f41689cbc175b22b23265735fd5
RomanBumburyak <romanbumburyak@gmail.com>
2017-10-05 16:31:18 -0400

    completed

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please choose one of the months");    /// using scanner to find on the months.
+        System.out.println("Please choose one of the months" );    /// using scanner to find on the months.
 
         for (Month month : Month.values()) {
             System.out.println(month.getEnglishName());
@@ -17,9 +17,31 @@ public class Main {
         }
         String userInput = scanner.nextLine();
 
+//        In an ArrayList add all the months for the given month and all the following months. This can be done many ways but an elegant solution can be coded using a switch statement.
+//
+//                Show the list to the user.
 
-
+        //(https://docs.oracle.com/javase/tutorial/java/nutsandbolts/switch.html)
         ArrayList<String> months = new ArrayList<>();
+
+
+        switch (userInput){
+            case "January": months.add("January");
+            case "February": months.add("February");
+            case "March": months.add("March");
+            case "April": months.add("April");
+            case "May": months.add("May");
+            case "June": months.add("June");
+            case "July": months.add("July");
+            case "August": months.add("August");
+            case "September": months.add("September");
+            case "October": months.add("October");
+            case "November": months.add("November");
+            case "December": months.add("December");
+
+            default: break;
+        }
+        System.out.println(months);
 
 
 
--- src/com/example/common/Month.java
+++ src/com/example/common/Month.java
@@ -5,30 +5,61 @@ public enum Month {
     ///enumeration for the days of the week:
 
 
-    JANUARY("JAN"),
-    FEBRUARY("FEB"),
-    MARCH("MARCH") ,
-    APRIL("APRIL"),
-    MAY("MAY") ,
-    JUNE( "JUNE"),
-    JULY("JULY"),
-    AUGUST( "AUGUST"),
-    SEPTEMBER("SEPTEMBER"),
-    OCTOBER("OCTOBER"),
-    NOVEMBER("NOVEMBER"),
-    DECEMBER("DECEMBER");
+    JANUARY("January", 1),
+    FEBRUARY("Febuary", 2),
+    MARCH("March", 3),
+    APRIL("April", 4),
+    MAY("May", 5) ,
+    JUNE( "June", 6),
+    JULY("July", 7),
+    AUGUST( "August", 8),
+    SEPTEMBER("September", 9),
+    OCTOBER("October", 10 ),
+    NOVEMBER("November", 11),
+    DECEMBER("December", 12);
 
-    private String englishName;
+    public String englishName;
+    public int monthNum;
 
-    Month(String englishName) {
+
+//    Month
+//
+//    Month(String englishName) {
+//        this.englishName = englishName;
+//    }
+//    Month(String monthNum) {this.monthNum = monthNum};
+
+    Month(String englishName, int monthNum) {
         this.englishName = englishName;
+        this.monthNum = monthNum;
     }
 
+
+
     ////This is my getter:
+//
+//    public String getEnglishName() {
+//        return englishName;
+//    }
+//
+//    Month(String englishName, int monthNum) {
+//        this.englishName = englishName;
+//        this.monthNum = monthNum;
+//    }
+//
+
 
     public String getEnglishName() {
         return englishName;
     }
+
+    public int getMonthNumber() {
+        return monthNum;
+    }
+
+
+
+
 
 
 
